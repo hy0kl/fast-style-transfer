7d8e1880ae601cc26a69e7b1d610d599dcd9190d
Jerry Yang <hy0kle@gmail.com>
2017-01-22 14:29:15 +0800

    fix for python3

--- src/optimize.py
+++ src/optimize.py
@@ -3,6 +3,7 @@ import vgg, pdb, time
 import tensorflow as tf, numpy as np, os
 import transform
 from utils import get_img
+from functools import reduce
 
 STYLE_LAYERS = ('relu1_1', 'relu2_1', 'relu3_1', 'relu4_1', 'relu5_1')
 CONTENT_LAYER = 'relu4_2'
@@ -88,7 +89,7 @@ def optimize(content_targets, style_target, content_weight, style_weight,
 
         # overall loss
         train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss)
-        sess.run(tf.initialize_all_variables())
+        sess.run(tf.global_variables_initializer())
         import random
         uid = random.randint(1, 100)
         print("UID: %s" % uid)
